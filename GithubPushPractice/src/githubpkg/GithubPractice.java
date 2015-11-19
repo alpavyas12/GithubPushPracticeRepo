00322e0145efa45fe0ab4cb9c8cb85b5ed1b0e32
alpavyas12 <alpavyas2010@yahoo.com>
2015-11-18 23:29:15 -0500

    added one line
    added one blank line

--- GithubPushPractice/src/githubpkg/GithubPractice.java
+++ GithubPushPractice/src/githubpkg/GithubPractice.java
@@ -1,6 +1,7 @@
 package githubpkg;
 
-public class GithubPractice {
+public class GithubPractice 
+{
 	public static void main(String[] args) {
 		System.out.println("I am practicing github push");
 	}
